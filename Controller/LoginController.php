c52d41bb146a13874c661bd097838f732d429ae2
Alan Hartless <alan@devkardia.com>
2020-11-10 21:53:47 -0600

    Replace removed security.context service

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -7,7 +7,9 @@ use Bazinga\OAuthServerBundle\Model\Provider\TokenProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\Security\Csrf\TokenStorage\TokenStorageInterface;
 use Symfony\Component\Templating\EngineInterface;
 
 /**
@@ -23,25 +25,26 @@ class LoginController
     protected $engine;
 
     /**
-     * @var SecurityContextInterface
+     * @var TokenStorageInterface
      */
-    protected $securityContext;
+    protected $tokenStorage;
+
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    protected $authorizationChecker;
 
     /**
      * @var TokenProviderInterface
      */
     protected $tokenProvider;
 
-    /**
-     * @param EngineInterface          $engine          The template engine.
-     * @param SecurityContextInterface $securityContext The security context.
-     * @param TokenProviderInterface   $tokenProvider   The OAuth token provider.
-     */
-    public function __construct(EngineInterface $engine, SecurityContextInterface $securityContext, TokenProviderInterface $tokenProvider)
+    public function __construct(EngineInterface $engine, TokenStorageInterface $tokenStorage, AuthorizationCheckerInterface $authorizationChecker, TokenProviderInterface $tokenProvider)
     {
-        $this->engine  = $engine;
-        $this->securityContext = $securityContext;
-        $this->tokenProvider   = $tokenProvider;
+        $this->engine               = $engine;
+        $this->tokenStorage         = $tokenStorage;
+        $this->authorizationChecker = $authorizationChecker;
+        $this->tokenProvider        = $tokenProvider;
     }
 
     /**
@@ -53,11 +56,11 @@ class LoginController
         $oauth_token    = $request->get('oauth_token', null);
         $oauth_callback = $request->get('oauth_callback', null);
 
-        if ($this->securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
+        if ($this->authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
             $token = $this->tokenProvider->loadRequestTokenByToken($oauth_token);
 
             if ($token instanceof RequestTokenInterface) {
-                $this->tokenProvider->setUserForRequestToken($token, $this->securityContext->getToken()->getUser());
+                $this->tokenProvider->setUserForRequestToken($token, $this->tokenStorage->getToken()->getUser());
 
                 return new Response($this->engine->render('BazingaOAuthServerBundle::authorize.html.twig', array(
                     'consumer'       => $token->getConsumer(),
